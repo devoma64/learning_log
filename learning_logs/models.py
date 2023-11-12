a18ff43ad4087777204d66231fd651be9566d9c4
devoma64 <devoma64@gmail.com>
2023-11-12 09:40:03 +0100

    Start building the learning app project, just finish the project setup and well defines urls

---
+++ learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        """Return a simple string representing the topic"""
+        return self.text
+    
+class Entry(models.Model):
+    """Something specific learn about a topic"""
+    topic = models.ForeignKey(Topic,  on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self) -> str:
+        """Return a simple string representing an entry"""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
+        
