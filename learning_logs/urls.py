89c66fbe66c0b30802d63c990978a5cf53498ada
devoma64 <devoma64@gmail.com>
2023-11-12 18:11:22 +0100

    implemented create entry and edit entry successfuly

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # individual topic page
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('create_topic/', views.create_topic, name='create_topic'),
+    path('create_entry/<int:topic_id>', views.create_entry, name='create_entry'),
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -30,3 +30,52 @@ def create_topic(request):
     # deplay a black or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/create_topic.html', context)
+
+def create_entry(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    if request.method != 'POST':
+        # No data submitted, display blank form
+        form = EntryForm()
+    else:
+        # Data submitted, process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            create_entry = form.save(commit=False)
+            create_entry.topic = topic
+            create_entry.save()
+            return redirect('learning_logs:topic', topic_id = topic_id)
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/create_entry.html', context)
+
+def edit_entry(request, entry_id):
+    # entry = Entry.objects.get(id = entry_id)
+    # topic = entry.topic
+    
+    # if request.method != 'POST':
+    #     # Initial request; pre-fill form with current entry.
+    #     form = EntryForm(instance=entry)
+    # else:
+    #     # POST data submitted; process data
+    #     form = EntryForm(instance = entry, data = request.POST)
+    #     print(form)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('learning_logs:topic', topic_id = topic.id)
+    
+    # context = {'entry': entry, 'topic': topic, 'from': form}
+    # return render(request, 'learning_logs/edit_entry.html', context)
+    
+    entry = Entry.objects.get(id = entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data= request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id = topic.id)
+        
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
