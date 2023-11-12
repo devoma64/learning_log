ca7f75d0726f21043f3ca7782f1629b2256458fe
devoma64 <devoma64@gmail.com>
2023-11-12 10:54:47 +0100

    finish creating the individual topic page to display individual topic and each entry associated with it. Implemented successfully

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,4 +5,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+        
+    # individual topic page
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,3 +8,8 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    return render(request, 'learning_logs/topic.html', {'topic': topic, 'entries': entries})
